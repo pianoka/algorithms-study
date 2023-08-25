9b947ecedc8cf54afa26cf7daf538fc137903bc6
pianoka <109780117+pianoka@users.noreply.github.com>
2023-08-26 02:58:11 +0900

    Update 쉬운 최단거리.py

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,20 +1,20 @@
 from collections import deque
 
-width, height = map(int, input().split())
+n, m = map(int, input().split())
 
-direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # 상하좌우
+direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 queue = deque([])
 
 input_arr = []
 visited_arr = []
 answer_arr = []
 
-for x in range(width):
+for x in range(n):
     input_row = list(map(int, input().split()))
     visited_row = []
     answer_row = []
 
-    for y in range(height):
+    for y in range(m):
         if input_row[y] == 2:
             queue.append((x, y))
             visited_row.append(True)
@@ -36,7 +36,7 @@ while queue:
     for dx, dy in direction:
         nx, ny = x + dx, y + dy
         
-        if 0 <= nx < width and 0 <= ny < height and not visited_arr[nx][ny] and input_arr[nx][ny] == 1:
+        if 0 <= nx < n and 0 <= ny < m and not visited_arr[nx][ny] and input_arr[nx][ny] == 1:
             queue.append((nx, ny))
             visited_arr[nx][ny] = True
             answer_arr[nx][ny] = answer_arr[x][y] + 1
